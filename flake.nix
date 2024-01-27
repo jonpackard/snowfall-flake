cdad685af7efe54c56da559592d39daa34da1fd9
Jonathan Packard <jwpackard@duck.com>
2024-01-27 00:11:25 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+        snowfall-lib = {
+            url = "github:snowfallorg/lib";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = inputs:
+        inputs.snowfall-lib.mkFlake {
+            # You must provide our flake inputs to Snowfall Lib.
+            inherit inputs;
+
+            # The `src` must be the root of the flake. See configuration
+            # in the next section for information on how you can move your
+            # Nix files to a separate directory.
+            src = ./.;
+        };
+}
