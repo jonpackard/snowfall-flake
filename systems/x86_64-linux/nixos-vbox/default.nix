762aa3c2f59e094f0e90cbe3e595f957877f237c
Jonathan Packard <jwpackard@duck.com>
2024-01-27 00:43:04 -0500

    disable osprober, enable vbox guest additions

--- systems/x86_64-linux/nixos-vbox/default.nix
+++ systems/x86_64-linux/nixos-vbox/default.nix
@@ -28,10 +28,12 @@
       ./hardware-configuration.nix
     ];
 
+  virtualisation.virtualbox.guest.enable = true;
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  # boot.loader.grub.useOSProber = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
