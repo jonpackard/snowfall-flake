c24e7e12a5cca2df4f7c94ad3ad68af544a64f71
Jonathan Packard <jwpackard@duck.com>
2024-02-01 21:43:38 -0500

    Add nvidia driver module

---
+++ modules/nixos/drivers/nvidia/default.nix
@@ -0,0 +1,65 @@
+{
+    # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+    # as well as the libraries available from your flake's inputs.
+    lib,
+    # An instance of `pkgs` with your overlays and packages applied is also available.
+    pkgs,
+    # You also have access to your flake's inputs.
+    inputs,
+
+    # Additional metadata is provided by Snowfall Lib.
+    system, # The system architecture for this host (eg. `x86_64-linux`).
+    target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+    format, # A normalized name for the system target (eg. `iso`).
+    virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
+    systems, # An attribute map of your defined hosts.
+
+    # All other arguments come from the module system.
+    config,
+    ...
+}:
+with lib; let
+  cfg = config.nvidia;
+in {
+  options.nvidia = {
+    enable = mkEnableOption "nvidia";
+  };
+
+  config = mkIf cfg.enable {
+    # Enable OpenGL
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Enable power management (do not disable this unless you have a reason to).
+      # Likely to cause problems on laptops and with screen tearing if disabled.
+      powerManagement.enable = false; # Changed from true to false to fix suspend issue
+    
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      # Do not disable this unless your GPU is unsupported or if you have a good reason to.
+      open = false; # SteamVR had terrible framerate with this set to true!
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+  };
+}
--- systems/x86_64-linux/jon-tower/default.nix
+++ systems/x86_64-linux/jon-tower/default.nix
@@ -39,6 +39,9 @@
   # Enable Gnome with X11 module
   gnome-x11.enable = true;
 
+  # Enable nvidia driver module
+  nvidia.enable = true;
+
   # Enable Flakes and the new command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -161,46 +164,6 @@
   };
   
   programs.gamemode.enable = true;
-
-  # ==START NVIDIA==
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Enable power management (do not disable this unless you have a reason to).
-    # Likely to cause problems on laptops and with screen tearing if disabled.
-    powerManagement.enable = false; # Changed from true to false to fix suspend issue
-	
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = false; # SteamVR had terrible framerate with this set to true!
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # ==END NVIDIA==
 
   virtualisation = {
       podman = {
