72c6277667f146c1d82ad922ae8b60b1a6d34b50
Jonathan Packard <jwpackard@duck.com>
2024-03-01 21:34:37 -0500

    Add zsh and fish to cli-apps

--- modules/home/cli-apps/default.nix
+++ modules/home/cli-apps/default.nix
@@ -42,6 +42,8 @@ in {
       pkgs.gnupg
       pkgs.fortune
       pkgs.neofetch
+      pkgs.zsh
+      pkgs.fish
       
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
