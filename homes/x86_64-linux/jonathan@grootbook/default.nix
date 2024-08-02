3431ce4a7d18857f0f6d3c94beec5bd1cb2b6798
Jonathan Packard <jwpackard@duck.com>
2024-08-02 03:33:57 +0000

    Add system: grootbook (2010 MacBook Pro... Yeah it's 14 years old!)

---
+++ homes/x86_64-linux/jonathan@grootbook/default.nix
@@ -0,0 +1,76 @@
+{
+    # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+    # as well as the libraries available from your flake's inputs.
+    lib,
+    # An instance of `pkgs` with your overlays and packages applied is also available.
+    pkgs,
+    # You also have access to your flake's inputs.
+    inputs,
+
+    # Additional metadata is provided by Snowfall Lib.
+    home, # The home architecture for this host (eg. `x86_64-linux`).
+    target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
+    format, # A normalized name for the home target (eg. `home`).
+    virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
+    host, # The host name for this home.
+
+    # All other arguments come from the home home.
+    config,
+    ...
+}:
+with lib;
+{
+  # Select home modules
+  gui-apps.enable = true;
+  cli-apps.enable = true;
+  cli-apps-linux.enable = true;
+
+  # Enable vscode-server service (external module)
+  # services.vscode-server.enable = true;
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "jonathan";
+  home.homeDirectory = "/home/jonathan";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/jonathan/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ systems/x86_64-linux/grootbook/default.nix
@@ -0,0 +1,153 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+    # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+    # as well as the libraries available from your flake's inputs.
+    lib,
+    # An instance of `pkgs` with your overlays and packages applied is also available.
+    pkgs,
+    # You also have access to your flake's inputs.
+    inputs,
+
+    # Additional metadata is provided by Snowfall Lib.
+    system, # The system architecture for this host (eg. `x86_64-linux`).
+    target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+    format, # A normalized name for the system target (eg. `iso`).
+    virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
+    systems, # An attribute map of your defined hosts.
+
+    # All other arguments come from the system system.
+    config,
+    ...
+}:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Enable TLP power management
+  services.power-profiles-daemon.enable = false;
+  services.tlp.enable = true;
+
+  # Enable Tailscale VPN module
+  tailscale.enable = true;
+
+  # Enable Gnome with X11 module
+  gnome-x11.enable = true;
+
+  # Enable pcloud app module
+  pcloud.enable = true;
+
+  # Enable Flakes and the new command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # networking.hostName = "jon-tower"; # Define your hostname. # needed removed for snowfall-lib
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    # keyMap = "us";
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jonathan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    # User packages defined in home-manager.
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.brandon = {
+    isNormalUser = true;
+    # extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    # User packages defined in home-manager.
+  };
+  
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    libimobiledevice # Needed for iPhone integration
+    ifuse # Needed for iPhone integration
+    home-manager
+    git
+    hplip
+    cryptsetup
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.settings.X11Forwarding = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Note: Anydesk uses port 7070/tcp+udp by default
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  programs.steam = {
+    enable = true;
+  };
+  
+  programs.gamemode.enable = true;
+
+  virtualisation = {
+      podman = {
+        enable = true;
+
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    };
+
+  services.usbmuxd.enable = true; # Needed for iPhone integration
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
